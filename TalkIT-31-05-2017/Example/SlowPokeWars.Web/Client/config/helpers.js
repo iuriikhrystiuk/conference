39728b3295010d247dc938862d7494e2bf6e2aa8
Iurii Khrystiuk <yura.1st.psy@gmail.com>
2017-05-09 21:45:09 +0300

    Fixing project

---
+++ TalkIT-31-05-2017/Example/SlowPokeWars.Web/Client/config/helpers.js
@@ -0,0 +1,31 @@
+﻿var path = require("path");
+
+const EVENT = process.env.npm_lifecycle_event || "";
+const baseFolder = "Client";
+
+var ROOT = path.resolve(__dirname, "../..");
+
+function hasProcessFlag(flag) {
+    return process.argv.join("").indexOf(flag) > -1;
+}
+
+function hasNpmFlag(flag) {
+    return EVENT.includes(flag);
+}
+
+function isWebpackDevServer() {
+    return process.argv[1] && !!(/webpack-dev-server/.exec(process.argv[1]));
+}
+
+function concatPath(location) {
+    return `./${baseFolder}/${location}`;
+}
+
+var root = path.join.bind(path, ROOT);
+
+exports.hasProcessFlag = hasProcessFlag;
+exports.hasNpmFlag = hasNpmFlag;
+exports.isWebpackDevServer = isWebpackDevServer;
+exports.root = root;
+exports.baseFolder = baseFolder;
+exports.concatPath = concatPath;
--- TalkIT-31-05-2017/Example/SlowPokeWars.Web/Client/src/app/boot.ts
+++ TalkIT-31-05-2017/Example/SlowPokeWars.Web/Client/src/app/boot.ts
@@ -1,6 +1,6 @@
 ﻿import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-import { AppComponent } from "./app/app";
+import { AppComponent } from "./app";
 
 @NgModule({
     imports: [BrowserModule],
